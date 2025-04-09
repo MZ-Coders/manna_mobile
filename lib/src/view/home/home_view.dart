ac5e7d0bf86b16a5393bd6a0c2301db1c600e2d8
Euler-JS <joaoalbertojose96@gmail.com>
2025-04-09 11:34:21 +0200

    Fetching all list

--- lib/src/view/home/home_view.dart
+++ lib/src/view/home/home_view.dart
@@ -231,42 +231,42 @@ class _HomeViewState extends State<HomeView> {
               const SizedBox(
                 height: 20,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Delivering to",
-                      style:
-                          TextStyle(color: TColor.secondaryText, fontSize: 11),
-                    ),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Current Location",
-                          style: TextStyle(
-                              color: TColor.secondaryText,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700),
-                        ),
-                        const SizedBox(
-                          width: 25,
-                        ),
-                        Image.asset(
-                          "assets/img/dropdown.png",
-                          width: 12,
-                          height: 12,
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(horizontal: 20),
+              //   child: Column(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       Text(
+              //         "Delivering to",
+              //         style:
+              //             TextStyle(color: TColor.secondaryText, fontSize: 11),
+              //       ),
+              //       const SizedBox(
+              //         height: 6,
+              //       ),
+              //       Row(
+              //         mainAxisAlignment: MainAxisAlignment.start,
+              //         children: [
+              //           Text(
+              //             "Current Location",
+              //             style: TextStyle(
+              //                 color: TColor.secondaryText,
+              //                 fontSize: 16,
+              //                 fontWeight: FontWeight.w700),
+              //           ),
+              //           const SizedBox(
+              //             width: 25,
+              //           ),
+              //           Image.asset(
+              //             "assets/img/dropdown.png",
+              //             width: 12,
+              //             height: 12,
+              //           )
+              //         ],
+              //       )
+              //     ],
+              //   ),
+              // ),
               const SizedBox(
                 height: 20,
               ),
@@ -304,10 +304,46 @@ class _HomeViewState extends State<HomeView> {
                   }),
                 ),
               ),
+              
+              // ListView.builder(
+              //   physics: const NeverScrollableScrollPhysics(),
+              //   shrinkWrap: true,
+              //   padding: EdgeInsets.zero,
+              //   itemCount: popArr.length,
+              //   itemBuilder: ((context, index) {
+              //     var pObj = popArr[index] as Map? ?? {};
+              //     return PopularRestaurantRow(
+              //       pObj: pObj,
+              //       onTap: () {},
+              //     );
+              //   }),
+              // ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: ViewAllTitleRow(
-                  title: "Popular Restaurants",
+                  title: "Most Popular",
+                  onView: () {},
+                ),
+              ),
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  itemCount: mostPopArr.length,
+                  itemBuilder: ((context, index) {
+                    var mObj = mostPopArr[index] as Map? ?? {};
+                    return MostPopularCell(
+                      mObj: mObj,
+                      onTap: () {},
+                    );
+                  }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ViewAllTitleRow(
+                  title: "Menu",
                   onView: () {},
                 ),
               ),
@@ -332,41 +368,6 @@ class _HomeViewState extends State<HomeView> {
                     },
                   );
                 }),
-              ),
-              ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                padding: EdgeInsets.zero,
-                itemCount: popArr.length,
-                itemBuilder: ((context, index) {
-                  var pObj = popArr[index] as Map? ?? {};
-                  return PopularRestaurantRow(
-                    pObj: pObj,
-                    onTap: () {},
-                  );
-                }),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: ViewAllTitleRow(
-                  title: "Most Popular",
-                  onView: () {},
-                ),
-              ),
-              SizedBox(
-                height: 200,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  itemCount: mostPopArr.length,
-                  itemBuilder: ((context, index) {
-                    var mObj = mostPopArr[index] as Map? ?? {};
-                    return MostPopularCell(
-                      mObj: mObj,
-                      onTap: () {},
-                    );
-                  }),
-                ),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
